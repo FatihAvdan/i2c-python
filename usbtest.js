52be55997aba2e2d4680e1740920cf87f01b069c
FatihAvdan <fatihavdan40@gmail.com>
2025-04-19 23:11:17 +0300

    changes

--- usbtest.js
+++ usbtest.js
@@ -4,11 +4,14 @@ const drivelist = require("drivelist");
 
 async function findVideoFile() {
   const drives = await drivelist.list();
+  console.log(drives);
   for (const drive of drives) {
     if (drive.isUSB) {
       const usbPath = drive.mountpoints[0]?.path;
+      console.log(usbPath);
       if (usbPath) {
         const videoPath = path.join(usbPath, "video.mp4");
+        console.log(videoPath);
         if (fs.existsSync(videoPath)) {
           return videoPath;
         }
